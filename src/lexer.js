fdf1a97dfdf6f3b2e7cf8f368dbce14db4ad0bc8
merdoth <ucheya97@gmail.com>
2026-02-13 04:07:21 +0100

    Add parser for numbers, booleans and null

--- src/lexer.js
+++ src/lexer.js
@@ -4,58 +4,89 @@ export function tokenize(input) {
   let i = 0;
 
   while (i < input.length) {
-    const character = input[i];
+    const char = input[i];
     
     // let's ignore whitespaces
-    if (character === " " || character === "\n" || character === "\t") {
+    if (char === " " || char === "\n" || char === "\t") {
       i++;
       continue;
     }
     
     // Characters
-    if (character === "{") {
+    if (char === "{") {
       tokens.push({ type: "LBRACE", value: "{" });
       i++;
       continue;
     }
 
-    if (character === "}") {
+    if (char === "}") {
       tokens.push({ type: "RBRACE", value: "}" });
       i++;
       continue; 
     }
 
     // Strings
-  if (character === '"') {
-    let value = "";
-    i++;
-
-    while (input[i] !== '"') {
-      if (i >= input.length) {
-        throw new Error("Unterminated string");
-      }
-      value += input[i];
+    if (char === '"') {
+      let value = "";
       i++;
+
+      while (input[i] !== '"') {
+        if (i >= input.length) {
+          throw new Error("Unterminated string");
+        }
+        value += input[i];
+        i++;
+      }
+
+      i++;
+      tokens.push({ type: "STRING", value });
+      continue;
     }
 
-    i++;
-    tokens.push({ type: "STRING", value });
-    continue;
-  }
+    if (char === ":") {
+      tokens.push({ type: "COLON", value: ":" });
+      i++;
+      continue;
+    }
 
-  if (character === ":") {
-    tokens.push({ type: "COLON", value: ":" });
-    i++;
-    continue;
-  }
+    if (char === ",") {
+      tokens.push({ type: "COMMA", value: "," });
+      i++;
+      continue;
+    }
 
-  if (character === ",") {
-    tokens.push({ type: "COMMA", value: "," });
-    i++;
-    continue;
-  }
+    // Numbers
+    if (/0-9/.test(char)) {
+      let num = "";
 
-    throw new Error('Unexpected character: ${character}');
+      while (/0-9/.test(input[i])) {
+        num += input[i];
+        i++;
+      }
+      tokens.push({ type: "NUMBER", value: Number(num)});
+    }
+
+    // Booleans
+    if (input.startsWith("true", i)) {
+      tokens.push({ type: "BOOLEAN", value: true });
+      i += 4;
+      continue;
+    }
+
+    if (input.startsWith("false", i)) {
+      tokens.push({ type: "BOOLEAN", value: false });
+      i += 5;
+      continue;
+    }
+
+    // Null
+    if (input.startsWith("null", i)) {
+      tokens.push({ type: "NULL", value: null });
+      i += 4;
+      continue;
+    }
+
+    throw new Error(`Unexpected character: ${char}`);
   }
 
   return tokens;
